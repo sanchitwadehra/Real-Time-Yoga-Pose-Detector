eff2f79ddba7973c56206e5ee76787e68d0b2f45
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-15 08:43:43 +0530

    video capture feature added

--- code/sketch.js
+++ code/sketch.js
@@ -1,7 +1,11 @@
+let capture;
+
 function setup() {
     createCanvas(400, 400);
+    capture = createCapture(VIDEO);
   }
   
   function draw() {
+    image(capture,0,0,800,600); 
     background(220);
   }
