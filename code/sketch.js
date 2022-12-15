2d9a017a0e2affa0bd373a971846a0e689dcadf1
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-15 22:56:16 +0530

    Update sketch.js

--- code/sketch.js
+++ code/sketch.js
@@ -1,35 +1,64 @@
-let capture;
-let posenet;
+let video;
+let poseNet;
 let pose;
-let poses;
+let skeleton;
 
 function setup(){
     createCanvas(800,600);
-    capture = createCapture(VIDEO);
-    capture.hide();
-    posenet=ml5.poseNet(capture,modelLoaded);
-    posenet.on('pose',receivedPoses);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
-function receivedPoses(poses){
+
+function gotPoses(poses){
     console.log(poses);
+    
     if(poses.length>0){
         pose=poses[0].pose;
+        skeleton=poses[0].skeleton;
     }
+    
 }
 
 function modelLoaded(){
-    console.log('Model has loaded');
+    console.log('poseNet ready');
 }
 
 function draw(){
-    image(capture,0,0,800,600);
+    image(video,0,0);
     //background(200);
-    if(pose){
-        FileList(255,0,0);
-    ellipse(pose.nose.x,pose.nose.y,64);
 
+    translate(video.width,0);
+    scale(-1,1);
+    image(video,0,0,video.width,video.height);
+    
+    if(pose){
+        let eyeR=pose.rightEye;
+        let eyeL=pose.leftEye;
+        let d=dist(eyeR.x,eyeR.y,eyeL.x,eyeL.y);
+    fill(0,0,0);
+    ellipse(pose.nose.x,pose.nose.y,d/2);
+    fill(0,0,0);
+    ellipse(pose.rightWrist.x,pose.rightWrist.y,16);
+    ellipse(pose.leftWrist.x,pose.leftWrist.y,16);
+
+    for(let i=0;i<pose.keypoints.length;i++){
+        let x=pose.keypoints[i].position.x;
+        let y=pose.keypoints[i].position.y;
+        fill(0,0,0);
+        ellipse(x,y,16,16);
     }
+    for(let i=0;i<skeleton.length;i++){
+        let a=skeleton[i][0];
+        let b=skeleton[i][1];
+        strokeWeight(2);
+        stroke(0);
+        line(a.position.x,a.position.y,b.position.x,b.position.y)
+    }
+    }
+    
     
 }
 
@@ -41,7 +70,7 @@ let pose;
 
 function setup() {
     createCanvas(1920, 1080);
-    video = createCapture(VIDEO);
+    video = createvideo(VIDEO);
     video.hide();
     poseNet=ml5.poseNet(video.modelLoaded);
     poseNet.on('pose',gotPoses);
