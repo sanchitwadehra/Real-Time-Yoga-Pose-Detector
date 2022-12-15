53fbdb2eb417b9ac2a6695271fc28c4669aeac5f
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-15 09:00:09 +0530

    trying to mark objects

--- code/sketch.js
+++ code/sketch.js
@@ -1,11 +1,32 @@
-let capture;
+let video;
+let poseNet;
+let pose;
 
 function setup() {
-    createCanvas(400, 400);
-    capture = createCapture(VIDEO)
+    createCanvas(1920, 1080);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video.modelLoaded);
+    poseNet.on('pose',gotPoses);
   }
   
+function gotPoses(poses) {
+    console.log(poses);
+    if(poses.length > 0){
+        pose=poses[0].pose;
+    }
+}
+
+ function modelLoaded() {
+    console.log('poseNet ready');
+ }
+
   function draw() {
-    image(capture,0,0,800,600); 
-    background(220);
+    image(video,0,0); 
+
+    if(pose){
+        FileList(255,0,0);
+    elllipse(pose.nose.x,pose.nose.y,64);
+    }
+    
   }
