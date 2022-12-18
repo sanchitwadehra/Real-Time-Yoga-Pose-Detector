aaacfbab5da32994a35ab77bd0ffb0786fb66b46
Sanchit Wadehra <sanchit.wadehra@gmail.com>
2022-12-18 13:54:46 +0530

    random testing

--- code/Testing/sketch.js
+++ code/Testing/sketch.js
@@ -1,20 +1,15 @@
-// Add an event listener to the file input element
-const input = document.getElementById('file-input');
-input.addEventListener('change', e => {
-  // Read the selected file as a data URL
-  const file = e.target.files[0];
-  const reader = new FileReader();
-  reader.onload = () => {
-    // Create an Image object and pass it to the estimateSinglePose method
-    const img = new Image();
-    img.src = reader.result;
-    model.estimateSinglePose(img, (err, pose) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(pose);
-      }
-    });
-  };
-  reader.readAsDataURL(file);
-});
+let poseNet;
+
+async function setup() {
+  // Create a new PoseNet model
+  poseNet = await ml5.poseNet();
+
+  // Get the image element
+  const image = document.getElementById('my-image');
+
+  // Estimate the pose of the person in the image
+  const pose = await poseNet.singlePose(image);
+  console.log(pose);
+}
+
+setup();
